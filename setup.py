1de337e21ddf05ba338f4dca44c6ed9d6022bff5
Chris Maillefaud <christian@geru.com.br>
2019-11-29 14:45:29 -0300

    Move shield colander schema to common

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ extras_require = {
 
 
 setup(name='gces',
-      version='0.0.12-alpha',
+      version='0.0.13',
       description='Google Cloud Event System.',
       long_description=long_description,
       long_description_content_type='text/markdown',
